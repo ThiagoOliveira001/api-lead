71d7afcea3c5f9b246a67b29c347f900b8339e2a
Thiago Oliveira <thiagosilvaoliveira66@gmail.com>
2021-12-08 13:06:14 -0300

    refactor: simplificando pasta de visita nos controllers

--- src/presentation/controllers/visita/contador-visita/contador-visita.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, HttpRequest, HttpResponse } from './contador-visita.protocols'
-import { ok } from '../../../helpers/http-helper'
-
-export class ContadorVisitaController implements Controller {
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return ok({ visitas: 1 })
-  }
-}
