4402bc05338c5b77762971c6b20fe6b33efa6756
Thiago Oliveira <thiagosilvaoliveira66@gmail.com>
2021-12-08 15:02:02 -0300

    test: adicionando tratamento de erro em ContadorVisitaController

--- src/presentation/controllers/visita/contador-visita.spec.ts
+++ src/presentation/controllers/visita/contador-visita.spec.ts
@@ -1,4 +1,5 @@
 import { RegistroVisita } from '../../../domain/models/RegistroVisita'
+import { ServerError } from '../../errors/server-error'
 import { ContadorVisitaController } from './contador-visita'
 import { AdicionarVisita } from './contador-visita.protocols'
 
@@ -46,5 +47,15 @@ describe('ContadorVisita Controller', () => {
     await sut.handle(httpRequest)
 
     expect(adicionarSpy).toHaveBeenCalled()
+  })
+
+  test('Deve retornar 500 quando ocorre um erro no usecase', async () => {
+    const { sut, adicionarVisitaStub } = makeSut()
+    jest.spyOn(adicionarVisitaStub, 'adicionar').mockRejectedValueOnce(new Error())
+    const httpRequest = {
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
