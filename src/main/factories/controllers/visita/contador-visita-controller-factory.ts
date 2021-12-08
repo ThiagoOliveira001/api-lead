eaf05dc0b1958308843b2bfd6a836b3a7bb0f955
Thiago Oliveira <thiagosilvaoliveira66@gmail.com>
2021-12-08 13:20:07 -0300

    refactor: simplificando pasta de visita na factor

---
+++ src/main/factories/controllers/visita/contador-visita-controller-factory.ts
@@ -0,0 +1,7 @@
+import { Controller } from '../../../../presentation/protocols'
+import { ContadorVisitaController } from '../../../../presentation/controllers/visita/contador-visita'
+
+export const makeContadorVisitaController = (): Controller => {
+  const controller = new ContadorVisitaController()
+  return controller
+}
--- src/main/routes/visita-route.ts
+++ src/main/routes/visita-route.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { adaptRoute } from '../adapters/express-route-adapter'
-import { makeContadorVisitaController } from '../factories/controllers/visita/contador-visita/contador-visita-controller-factory'
+import { makeContadorVisitaController } from '../factories/controllers/visita/contador-visita-controller-factory'
 import { Router } from 'express'
 
 export default (router: Router): any => {
