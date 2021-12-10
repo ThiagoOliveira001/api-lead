ea9d12afcae71e779389c3511a24bdc109bcc92a
Thiago Oliveira <thiagosilvaoliveira66@gmail.com>
2021-12-10 01:48:37 -0300

    feat: adicionando rotas de usuario ao roteamento

--- src/main/routes.ts
+++ src/main/routes.ts
@@ -1,9 +1,11 @@
 import { Express, Router } from 'express'
 import visitaRoute from './routes/visita-route'
+import usuarioRoute from './routes/usuario-route'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api/v1', router)
 
   visitaRoute(router)
+  usuarioRoute(router)
 }
