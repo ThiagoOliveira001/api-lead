93bf68381ea9877d91d4a95fcc5ffbcc194d1f43
Thiago Oliveira <thiagosilvaoliveira66@gmail.com>
2021-12-10 01:36:33 -0300

    refactor: assegurando atributo message no ServerError

--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -2,5 +2,6 @@ export class ServerError extends Error {
   constructor () {
     super('Ops, tivemos um pequeno problema, tente novamente mais tarde.')
     this.name = 'ServerError'
+    this.message = 'Ops, tivemos um pequeno problema, tente novamente mais tarde.'
   }
 }
