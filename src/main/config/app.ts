cb37ac4204077681783b2eeba541088313a84111
Thiago Oliveira <thiagosilvaoliveira66@gmail.com>
2021-12-10 01:49:35 -0300

    feat: inicializando banco e tratando requisicoes pelo express

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,10 +2,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 import setupRoute from '../routes'
 import setupSwagger from './swagger'
+import setupDatabase from './database'
 
 dotenv.config()
 const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 setupRoute(app)
 setupSwagger(app)
+setupDatabase()
 
 export default app
